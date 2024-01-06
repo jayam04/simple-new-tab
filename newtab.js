084d448f0780780cce15c760bb34f732ab2d775e
jayam04 <hello@crimsontwilight.maskmy.id>
2024-01-06 12:19:48 +0530

    first commit

---
+++ newtab.js
@@ -0,0 +1,16 @@
+function updateClockNewTab() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  document.getElementById('digitalClockNewTab').innerText = timeString;
+}
+
+// Update the clock every second
+setInterval(updateClockNewTab, 1000);
+
+// Initial update
+updateClockNewTab();
+
