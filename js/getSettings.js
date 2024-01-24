12d4a7908a0bb283277da7e984d3b84d1e8f091c
jayam04 <hello@crimsontwilight.maskmy.id>
2024-01-24 22:17:18 +0530

    updated icons

--- js/getSettings.js
+++ js/getSettings.js
@@ -1,3 +1,26 @@
-function isChromeSyncOn() {
-    return localStorage.getItem("isChromeSyncOn") === "true";
+import {IS_AUTO_SAVE_ON, IS_CHROME_SYNC_ON, SYNC_SETTINGS_IN_REALTIME} from "./constants.mts";
+
+let chromeSync :boolean;
+let autoSave :boolean;
+let settingsSync :boolean;
+
+export function isChromeSyncOn() :boolean {
+    if (chromeSync === undefined) {
+        chromeSync = !(localStorage.getItem(IS_CHROME_SYNC_ON) === "false");
+    }
+    return chromeSync;
+}
+
+export function isAutoSaveOn() :boolean {
+    if (autoSave === undefined) {
+        autoSave = !(localStorage.getItem(IS_AUTO_SAVE_ON) === "false");
+    }
+    return autoSave;
+}
+
+export function syncSettingsInRealtime() :boolean {
+    if (settingsSync === undefined) {
+        settingsSync = !(localStorage.getItem(SYNC_SETTINGS_IN_REALTIME) === "false");
+    }
+    return settingsSync;
 }
