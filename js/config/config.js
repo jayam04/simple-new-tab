7342888bbe262f8a95a317cc023c7449f7264c02
Ayush Kavad <ayushkavad@gmail.com>
2024-01-25 19:31:03 +0530

    refactor code

---
+++ js/config/config.js
@@ -0,0 +1,2 @@
+export const DEFAULT_TAB_NAME = 'Simple New Tab ✨';
+export const GOOGLE_FONT_BASE_URL = 'https://fonts.googleapis.com/css2';
--- js/getSettings.js
+++ js/getSettings.js
@@ -1,26 +1,32 @@
-import {IS_AUTO_SAVE_ON, IS_CHROME_SYNC_ON, SYNC_SETTINGS_IN_REALTIME} from "./constants.mts";
+import {
+  IS_AUTO_SAVE_ON,
+  IS_CHROME_SYNC_ON,
+  SYNC_SETTINGS_IN_REALTIME,
+} from './constants.mts';
 
-let chromeSync :boolean;
-let autoSave :boolean;
-let settingsSync :boolean;
+let chromeSync;
+let autoSave;
+let settingsSync;
 
-export function isChromeSyncOn() :boolean {
-    if (chromeSync === undefined) {
-        chromeSync = !(localStorage.getItem(IS_CHROME_SYNC_ON) === "false");
-    }
-    return chromeSync;
+export function isChromeSyncOn() {
+  if (chromeSync === undefined) {
+    chromeSync = !(localStorage.getItem(IS_CHROME_SYNC_ON) === 'false');
+  }
+  return chromeSync;
 }
 
-export function isAutoSaveOn() :boolean {
-    if (autoSave === undefined) {
-        autoSave = !(localStorage.getItem(IS_AUTO_SAVE_ON) === "false");
-    }
-    return autoSave;
+export function isAutoSaveOn() {
+  if (autoSave === undefined) {
+    autoSave = !(localStorage.getItem(IS_AUTO_SAVE_ON) === 'false');
+  }
+  return autoSave;
 }
 
-export function syncSettingsInRealtime() :boolean {
-    if (settingsSync === undefined) {
-        settingsSync = !(localStorage.getItem(SYNC_SETTINGS_IN_REALTIME) === "false");
-    }
-    return settingsSync;
+export function syncSettingsInRealtime() {
+  if (settingsSync === undefined) {
+    settingsSync = !(
+      localStorage.getItem(SYNC_SETTINGS_IN_REALTIME) === 'false'
+    );
+  }
+  return settingsSync;
 }
