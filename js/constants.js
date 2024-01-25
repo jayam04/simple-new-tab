69f35fea1baccdcfff87a69353fd00fbfcf47d7a
jayam04 <hello@crimsontwilight.maskmy.id>
2024-01-25 23:53:57 +0530

    few updates.

---
+++ js/constants.js
@@ -0,0 +1,12 @@
+// extension related
+export const EXTENSION_NAME = "Simple New Tab ✨";
+
+// directories
+export const PASTEL_PATTERN_BACKGROUND_DIR = "/img/pastel-patterns";
+export const NO_OF_PASTEL_PATTERN_BACKGROUND_FILES = 4;
+export const NOISE_PATTERN_DIR = "/img/noise";
+
+// local storage constants
+export const SYNC_SETTINGS_IN_REALTIME = "syncSettingsInRealtime";
+export const IS_AUTO_SAVE_ON = "isAutoSaveOn";
+export const IS_CHROME_SYNC_ON = "isChromeSyncOn";
