3e3fbeede56d7ba0c405d378a48e920e38c7463a
jayam04 <hello@crimsontwilight.maskmy.id>
2024-01-28 11:43:50 +0530

    added new constants file

---
+++ migration/js/constants.js
@@ -0,0 +1,42 @@
+// Names
+export const SECTIONS = [
+    {
+        "id": "visual",
+        "name": "🎨 Visual",
+        "headingName": "🎨 Visual Settings"
+    },
+    {
+        "id": "behavioral",
+        "name": "🔧 Behavioral",
+        "headingName": "🔧 Behavioral Settings"
+    },
+    {
+        "id": "extension",
+        "name": "⚙️ Extension",
+        "headingName": "⚙️ Extension  Settings ✨"
+    },
+    {
+        "id": "about-us",
+        "name": "👥 About Us",
+        "headingName": "👥 About ✨"
+    }
+]
+
+// Colors
+export const COLORS = {
+    "primary": "#0097B2",
+    "secondary": "#7ED957",
+    "tertiary": "#ffffff"
+}
+
+// Defaults
+export const DEFAULT_SETTINGS = {
+    // Extension
+    "chromeSync": true,
+
+    // Visual
+    "theme": "light",
+    "noise": "high"
+
+    // Behavioral
+}
